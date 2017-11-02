d981cae9f04cf78a36c24372eeaaef724c614a64
Scott McAllister <stmcallister@gmail.com>
2017-11-02 14:09:55 -0700

    added search command

---
+++ sscli-search.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const search = require('./lib/search.js');
+var program = require('commander');
+console.log('sscli-search--before program');
+
+program
+    .action(function() {
+        const info = program.args[program.args.length-1];
+        search.searchAll(info);
+    });
+
+program.parse(process.argv);
+    
