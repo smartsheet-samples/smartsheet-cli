4e1917ec80c3358502bd8aeff55042dedafe30e0
Taylor Krusen <tkrusen@bel-mbp-tkrusen.apollo.smartsheet.com>
2017-11-02 12:25:23 -0700

    working on auth

--- sscli-auth.js
+++ sscli-auth.js
@@ -1,9 +1,20 @@
 #!/usr/bin/env node
 const auth = require('./lib/auth.js');
-var program = require('commander');
+const program = require('commander');
 
 program
     .command('login')
     .action(function() {
-        auth.gotAuth();
+        auth.login();
     });
+
+program
+    .command('logout')
+    // options arent getting passed corrrectly with the way auth.js is structured
+    .option('-f, --force', 'Force logout')
+    .description('Removes your Smartsheet access token.')
+    .action(function() {
+        console.log("i'm calling logout")
+        auth.logout();
+    });
+
